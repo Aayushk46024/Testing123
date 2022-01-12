2c7320a4f9600ea01d13b5eccf2214928106ed5c
AayushKhandelwal <aayush.khandelwal@infostride.com>
2022-01-12 12:54:39 +0530

    Initial commit

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args){
-        System.out.println("Hello, welcome to the infostride");
+        System.out.println("Hello, Welcome to the Infostride!");
     }
 }
